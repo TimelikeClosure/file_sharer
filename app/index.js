3ecfbfc5ffdc201a0f490a62d1139c80889799cd
Tim Horist <TimelikeClosure@users.noreply.github.com>
2019-06-19 16:56:52 -0700

    Fixed import issue

--- app/index.js
+++ app/index.js
@@ -3,4 +3,4 @@
 const { resolve } = require('path');
 
 const config = require(resolve('.', 'config'));
-const utils = require(resolve(__dirname, 'utils'));
+const utils = require(resolve('.', 'utils'));
