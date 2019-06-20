52835b26c5f7e6299e249d849b357cffe96350f0
Tim Horist <TimelikeClosure@users.noreply.github.com>
2019-06-19 17:18:29 -0700

    Set up basic webserver

--- app/index.js
+++ app/index.js
@@ -4,3 +4,18 @@ const { resolve } = require('path');
 
 const config = require(resolve('.', 'config'));
 const utils = require(resolve('.', 'utils'));
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(resolve('.', 'public')));
+
+app.get('*', (req, res) => {
+    res.sendFile(resolve(__dirname, 'public', 'index.html'));
+});
+
+const PORT = config.env.PORT || 4321;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
